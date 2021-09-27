1312ec3ba599abd41d0037c43605fabb0815520c
1с <1@ZCORP>
2021-09-27 10:26:52 +0300

    Добавьте файлы проекта.

---
+++ HomeWork_2_7/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace HomeWork_2_7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //список студентов
+            List<Student> students = new List<Student>();
+            students.Add(new Student("Иванов И.И.", 25, "mail@mail.ru", 25, 35, 44));
+            students.Add(new Student("Петров П.П.", 23, "mail@list.ru", 85, 65, 32));
+
+             //форматированный вывод списка студенов задание 1 
+            foreach (Student studend in students)
+            {
+                studend.Print();
+            }
+
+            Console.ReadKey();
+
+            //форматированный вывод списка студенов задание 2 
+            foreach (Student studend in students)
+            {
+                studend.PrintAverageScore();
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ HomeWork_2_7/Student.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork_2_7
+{
+    /// <summary>
+    /// Студенты
+    /// </summary>
+    class Student
+    {
+        private string fullName { get; set; }
+        private int age { set; get; }
+        private string email { set; get; }
+        private double scoreProgram { set; get; }
+        private double scoreMaths { set; get; }
+        private double scorePhysics { set; get; }
+
+        private double averageScore { set; get; }
+
+        public Student (string fullName, int age, string email, double scoreProgram, double scoreMaths , double scorePhysics)
+        {
+            this.fullName = fullName;
+            this.age = age;
+            this.email = email;
+            this.scoreMaths = scoreMaths;
+            this.scorePhysics = scorePhysics;
+            this.scoreProgram = scoreProgram;
+            double score = (scoreMaths + scorePhysics + scoreProgram) / 3; 
+            this.averageScore = score; 
+        }
+        public void PrintAverageScore()
+        {
+            Console.WriteLine($"{fullName} у студента средний бал : {averageScore}");
+        }
+        public void Print()
+        {
+            Console.WriteLine($"{fullName, 15} {age, 8} {email, 15} {scoreProgram, 15} {scoreMaths,15} {scorePhysics, 15}");
+        }
+    }
+}
